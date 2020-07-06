5160861b19624224a4ef2eb77a3aca22bf491679
Yaqubi1 <Yaqubi1_1@users.noreply.github.com>
2020-07-07 03:30:44 +0430

    mediaQ menu hover

--- script.js
+++ script.js
@@ -3,17 +3,12 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    // document.getElementById("navbar-container").style.opacity = 1;
     document.getElementById("navbar-container").style.top = "0px";
   } else {
-    // document.getElementById("navbar-container").style.opacity = 0.1;
-    document.getElementById("navbar-container").style.top = "-50px";
+    document.getElementById("navbar-hover").style.top = "-50px";
+    if (document.getElementById("navbar-container").style.height = "100px") {
+      document.getElementById("navbar-container").style.top = "-100px";
+    }
   }
   prevScrollpos = currentScrollPos;
 }
-
-//  function2() {
-//     if (document.getElementById("navbar-container").style.top = "-50px";) {
-//      document.getElementById("navbar-container").style.top = "0px";
-//     }
-//  }
