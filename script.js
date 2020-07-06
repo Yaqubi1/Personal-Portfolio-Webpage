46904a7a2d563382f96217301fd9e553b46ba6cc
Yaqubi1 <Yaqubi1_1@users.noreply.github.com>
2020-07-07 04:02:15 +0430

    Fixed menu hover

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar-container").style.top = "0px";
   } else {
-    document.getElementById("navbar-hover").style.top = "-50px";
+    document.getElementById("navbar-container").style.top = "-50px";
     if (document.getElementById("navbar-container").style.height = "100px") {
       document.getElementById("navbar-container").style.top = "-100px";
     }
