ea14e429b870934d3456af9d32865d63e622ee66
Yaqubi1 <Yaqubi1_1@users.noreply.github.com>
2020-07-07 05:00:22 +0430

    js click menu disappear

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onscroll = function() {
     document.getElementById("navbar-container").style.top = "0px";
   } else {
     document.getElementById("navbar-container").style.top = "-50px";
-    if (document.getElementById("navbar-container").style.height = "100px") {
+    if (window.innerWidth < 732) {
       document.getElementById("navbar-container").style.top = "-100px";
     }
   }
